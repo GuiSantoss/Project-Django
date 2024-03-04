4899475e58ac647626d0ec52b27eb9b5ef053044
Guilherme Santos <gh.vieirasantos@gmail.com>
2024-03-04 16:44:05 -0300

    Forms, tests and organizing files

--- project/settings.py
+++ project/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # A gente tem apps também
     'recipes',
+    'authors',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- utils/test_paginations.py
+++ utils/test_paginations.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from utils.pagination import make_pagination_range
 
 class PaginationTest(TestCase):
-    def test_pagination_range_returns_a_pagination_range(self):
+    def test_make_pagination_range_returns_a_pagination_range(self):
         pagination = make_pagination_range(
             page_range = list(range(1, 21)),
             qty_pages=4,
@@ -104,3 +104,6 @@ class PaginationTest(TestCase):
             current_page=21,
         )['pagination']
         self.assertEqual([17,18,19,20], pagination)
+
+
+
