8af7543ad00435755072322ee5cf04e5d7587cb4
Guilherme Santos <gh.vieirasantos@gmail.com>
2024-04-09 18:06:51 -0300

    Class Based View and fixing testings

--- recipes/tests/test_recipe_category_view.py
+++ recipes/tests/test_recipe_category_view.py
@@ -9,7 +9,7 @@ class RecipeCategoryViewTest(RecipeTestBase):
         view = resolve(
             reverse('recipes:category', kwargs={'category_id': 1000})
         )
-        self.assertIs(view.func, views.category)
+        self.assertIs(view.func.view_class, views.RecipeListViewCategory)
 
 
     def test_recipe_category_view_returns_404_if_no_recipes_found(self):
@@ -36,7 +36,7 @@ class RecipeCategoryViewTest(RecipeTestBase):
         recipe = self.make_recipe(is_published=False)
 
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': recipe.id})
+            reverse('recipes:recipe', kwargs={'pk': recipe.id})
         )
 
         self.assertEqual(response.status_code, 404)
--- recipes/tests/test_recipe_detail_view.py
+++ recipes/tests/test_recipe_detail_view.py
@@ -7,14 +7,14 @@ class RecipeDetailViewTest(RecipeTestBase):
 
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
-            reverse('recipes:recipe', kwargs={'id': 1})
+            reverse('recipes:recipe', kwargs={'pk': 1})
         )
-        self.assertIs(view.func, views.recipe)
+        self.assertIs(view.func.view_class, views.RecipeDetail)
 
 
     def test_recipe_detail_view_returns_404_if_no_recipes_found(self):
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1000})
+            reverse('recipes:recipe', kwargs={'pk': 1000})
         )
         self.assertEqual(response.status_code, 404)
 
@@ -27,7 +27,7 @@ class RecipeDetailViewTest(RecipeTestBase):
             reverse(
                 'recipes:recipe', 
         kwargs={
-            'id' : 1
+            'pk' : 1
         }))
         content = response.content.decode('utf-8')
 
@@ -44,7 +44,7 @@ class RecipeDetailViewTest(RecipeTestBase):
             reverse(
                 'recipes:recipe', 
         kwargs={
-            'id' : recipe.id
+            'pk' : recipe.id
         }))
         self.assertEqual(response.status_code, 404)
 
